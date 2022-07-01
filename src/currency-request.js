53098a7796fe96ded0eb4b5bc76cbad282bddafd
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-01 10:37:17 -0700

    WIP debugging API request

--- src/currency-request.js
+++ src/currency-request.js
@@ -1,7 +1,7 @@
 export class CurrencyRequest {
-  static async getCurrency() {
+  static async getCurrency(currencyType, currencyConvert) {
     try {
-      const response = await fetch(``);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/10801311019cb9145181614b/pair/${currencyType}/${currencyConvert}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import $ from 'jquery';
 import { CurrencyRequest } from './currency-request';
 
 function getElementsCurrency(response) {
-  let usd = response;
-  $('#exchangeOutput').html(``)
+  $('#exchangeOutput').html(response);
 }
 
-async function makeCurrencyApiCall(usd, currecncyType){
-  const response = await CurrencyRequest.getCurrency(exchange);
+async function makeCurrencyApiCall(currencyType, currencyConvert){
+  const response = await CurrencyRequest.getCurrency(currencyType, currencyConvert);
   getElementsCurrency(response);
 }
 
 $(document).ready(function(){
   $('#currencyExchanger').submit(function(event){
     event.preventDefault();
-    let usd = $('#usd').val();
-    let currecncyType = $('#currencyType').val();
-  })
-})
+    let currencyType = $('#currencyType').val();
+    let currencyConvert = $('#currencyConvert').val();
+    makeCurrencyApiCall(currencyType, currencyConvert);
+  });
+});
