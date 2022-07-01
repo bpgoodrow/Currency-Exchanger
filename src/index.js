2c9d028ceaaaf9f27840225842c1fcf3231d4f4c
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-01 08:47:00 -0700

    index.js shell built

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import { CurrencyRequest } from './currency-request';
+
+function getElementsCurrency(response) {
+  let exchange = response;
+  $('#exchangeOutput').html(``)
+}
+
+async function makeCurrencyApiCall(exchange){
+  const response = await CurrencyRequest.getCurrency(exchange);
+  getElementsCurrency(response);
+}
+
+$(document).ready(function(){
+  $('#currencyExchanger').submit(function(event){
+    event.preventDefault();
+    let exchange = $('#exchange').val();
+  })
+})
