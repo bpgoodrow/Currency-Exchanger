14b0a8971358a5840d4e9c47934b2866f972fa47
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-01 16:26:21 -0700

    API key in .env working

--- README.md
+++ README.md
@@ -25,12 +25,12 @@ This is a web application that uses the www.exchangerate-api.com API to return e
 * In browser navigate to https://www.exchangerate-api.com/docs/overview and follow instructions to create AIP key.
 * Create .env file in root directory.
 * add API key to .env file $ API_KEY=YOUR-API-KEY-HERE without any spaces, backticks or quotes. Its not a string.
+* add .env to .gitignore file.
 * add API key to API url with ${process.env.API_KEY}.
 
 
 ## Known Bugs
 * API key in .env file returns 403 error.
-* 3 digit currency letter code must be typed in caps.
 
 ## License
 MIT
--- src/currency-request.js
+++ src/currency-request.js
@@ -1,12 +1,12 @@
 export class CurrencyRequest {
   static async getCurrency(currencyType, currencyConvert, exchangeAmount) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/10801311019cb9145181614b/pair/${currencyType}/${currencyConvert}/${exchangeAmount}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyType}/${currencyConvert}/${exchangeAmount}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
-    } catch(error) {
+    } catch (error) {
       return error.message;
     }
   }
